a4b3a6daa434edcf1ab0e1be9204e2bc13cdeeeb
SpencerHaslam <71406886+SpencerHaslam@users.noreply.github.com>
2020-10-06 11:08:52 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -21,7 +21,7 @@ example:
 Enter a name: Lebron
 That name is on the list
 """
-name = str(input("Enter a name").stip())
+name = str(input("Enter a name")).strip()
 nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
 for x in nameList:
     
